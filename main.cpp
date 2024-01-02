f8e35775f64e13a152bb2282adb527e66ccec9f1
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2024-01-02 21:37:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<conio.h>
+#include<dos.h>
+#include <windows.h>
+#include<time.h>
+
+// Directories
+#include "MainHeader.h"
+
+using namespace std;
+
+
+
+
+int main() {
+	
+	getMenu();
+	
+	
+}
+
+
